9c3186ae21dab018b499adea50f50471e0bcedc3
hyeyeon059 <kimhyeyeon2006@gmail.com>
2022-07-13 20:13:17 +0900

    기린 스킬 구현

--- Assets/01.Scripts/Dev/Hyeyeon/Enemy.cs
+++ Assets/01.Scripts/Dev/Hyeyeon/Enemy.cs
@@ -4,37 +4,14 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
-    public float speed;
-    public int health;
-    public Sprite[] sprites;
-
-    SpriteRenderer spriteRenderer;
-    Rigidbody2D rb;
 
     void Start()
     {
-        
-    }
 
-    private void Awake()
-    {
-        spriteRenderer = GetComponent<SpriteRenderer>();
-        rb = GetComponent<Rigidbody2D>();
-        rb.velocity = Vector2.down * speed;
     }
 
     void Update()
     {
-        
-    }
 
-    private void OnHit(int dmg)
-    {
-        health -= dmg;
-
-        if(health <= 0)
-        {
-            Destroy(gameObject);
-        }
     }
 }
