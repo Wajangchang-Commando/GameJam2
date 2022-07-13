4dc444c8e80a2e5f7f517724ab66463db71836cd
Dongyeop <kitty-777@naver.com>
2022-07-13 11:31:21 +0900

    여러가지

--- Assets/01.Scripts/Dev/Dongyeop/Player/PlayerSkillChanger.cs
+++ Assets/01.Scripts/Dev/Dongyeop/Player/PlayerSkillChanger.cs
@@ -19,8 +19,8 @@ public class PlayerSkillChanger : MonoBehaviour // Q, E¸¦ ´©¸£¸é »ç¿ë °¡´É ½ºÅ³À
     [SerializeField] private GameObject _eKnife;
     #endregion
 
-    private int _nowNum = 1;
-    private int qNum = 2;
+    public int nowNum = 1;
+    private int _qNum = 2;
     private int _eNum = 3;
     private int _reset = 0;
 
@@ -35,59 +35,59 @@ public class PlayerSkillChanger : MonoBehaviour // Q, E¸¦ ´©¸£¸é »ç¿ë °¡´É ½ºÅ³À
     #region ÇÏµåÄÚµù.
     private void InputQ()
     {
-        switch (_nowNum)
+        switch (nowNum)
         {
             case 1:
-                if (qNum == 2)
+                if (_qNum == 2)
                 {
                     Helpme(_nowGun, _nowBow, _qBow, _qGun);
-                    _reset = _nowNum;
-                    _nowNum = qNum;
-                    qNum = _reset;
+                    _reset = nowNum;
+                    nowNum = _qNum;
+                    _qNum = _reset;
                     _reset = 0;
                 }
-                else if (qNum == 3)
+                else if (_qNum == 3)
                 {
                     Helpme(_nowGun, _nowKnife, _qKnife, _qGun);
-                    _reset = _nowNum;
-                    _nowNum = qNum;
-                    qNum = _reset;
+                    _reset = nowNum;
+                    nowNum = _qNum;
+                    _qNum = _reset;
                     _reset = 0;
                 }
                 break;
             case 2:
-                if(qNum == 1)
+                if(_qNum == 1)
                 {
                     Helpme(_nowBow, _nowGun, _qGun, _qBow);
-                    _reset = _nowNum;
-                    _nowNum = qNum;
-                    qNum = _reset;
+                    _reset = nowNum;
+                    nowNum = _qNum;
+                    _qNum = _reset;
                     _reset = 0;
                 }
-                else if (qNum == 3)
+                else if (_qNum == 3)
                 {
                     Helpme(_nowBow, _nowKnife, _qKnife, _qBow);
-                    _reset = _nowNum;
-                    _nowNum = qNum;
-                    qNum = _reset;
+                    _reset = nowNum;
+                    nowNum = _qNum;
+                    _qNum = _reset;
                     _reset = 0;
                 }
                 break;
             case 3:
-                if (qNum == 1)
+                if (_qNum == 1)
                 {
                     Helpme(_nowKnife, _nowGun, _qGun, _qKnife);
-                    _reset = _nowNum;
-                    _nowNum = qNum;
-                    qNum = _reset;
+                    _reset = nowNum;
+                    nowNum = _qNum;
+                    _qNum = _reset;
                     _reset = 0;
                 }
-                else if (qNum == 2)
+                else if (_qNum == 2)
                 {
                     Helpme(_nowKnife, _nowBow, _qBow, _qKnife);
-                    _reset = _nowNum;
-                    _nowNum = qNum;
-                    qNum = _reset;
+                    _reset = nowNum;
+                    nowNum = _qNum;
+                    _qNum = _reset;
                     _reset = 0;
                 }
                 break;
@@ -98,22 +98,22 @@ public class PlayerSkillChanger : MonoBehaviour // Q, E¸¦ ´©¸£¸é »ç¿ë °¡´É ½ºÅ³À
 
     private void InputE()
     {
-        switch (_nowNum)
+        switch (nowNum)
         {
             case 1:
                 if (_eNum == 2)
                 {
                     Helpme(_nowGun, _nowBow, _eBow, _eGun);
-                    _reset = _nowNum;
-                    _nowNum = _eNum;
+                    _reset = nowNum;
+                    nowNum = _eNum;
                     _eNum = _reset;
                     _reset = 0;
                 }
                 else if (_eNum == 3)
                 {
                     Helpme(_nowGun, _nowKnife, _eKnife, _eGun);
-                    _reset = _nowNum;
-                    _nowNum = _eNum;
+                    _reset = nowNum;
+                    nowNum = _eNum;
                     _eNum = _reset;
                     _reset = 0;
                 }
@@ -122,16 +122,16 @@ public class PlayerSkillChanger : MonoBehaviour // Q, E¸¦ ´©¸£¸é »ç¿ë °¡´É ½ºÅ³À
                 if (_eNum == 1)
                 {
                     Helpme(_nowBow, _nowGun, _eGun, _eBow);
-                    _reset = _nowNum;
-                    _nowNum = _eNum;
+                    _reset = nowNum;
+                    nowNum = _eNum;
                     _eNum = _reset;
                     _reset = 0;
                 }
                 else if (_eNum == 3)
                 {
                     Helpme(_nowBow, _nowKnife, _eKnife, _eBow);
-                    _reset = _nowNum;
-                    _nowNum = _eNum;
+                    _reset = nowNum;
+                    nowNum = _eNum;
                     _eNum = _reset;
                     _reset = 0;
                 }
@@ -140,16 +140,16 @@ public class PlayerSkillChanger : MonoBehaviour // Q, E¸¦ ´©¸£¸é »ç¿ë °¡´É ½ºÅ³À
                 if (_eNum == 1)
                 {
                     Helpme(_nowKnife, _nowGun, _eGun, _eKnife);
-                    _reset = _nowNum;
-                    _nowNum = _eNum;
+                    _reset = nowNum;
+                    nowNum = _eNum;
                     _eNum = _reset;
                     _reset = 0;
                 }
                 else if (_eNum == 2)
                 {
                     Helpme(_nowKnife, _nowBow, _eBow, _eKnife);
-                    _reset = _nowNum;
-                    _nowNum = _eNum;
+                    _reset = nowNum;
+                    nowNum = _eNum;
                     _eNum = _reset;
                     _reset = 0;
                 }
