fce4b9ad6bac85f64bfc7670ddf3b4ad097d571d
Dongyeop <kitty-777@naver.com>
2022-07-14 11:36:05 +0900

    aishiteru

--- Assets/01.Scripts/Dev/Dongyeop/UI/StatsUpgrade.cs
+++ Assets/01.Scripts/Dev/Dongyeop/UI/StatsUpgrade.cs
@@ -8,48 +8,77 @@ public class StatsUpgrade : MonoBehaviour
     private PlayerMove _playerMove;
     private Bow _bow;
     private Gun _gun;
-    private Bullet _bullet;
     private PlayerHP _playerHP;
+
+    private int _knifeDam = 0;
+    public int _knifeADV = 0;
+    private int _bowMin = 0;
+    private int _bowMax = 0;
+    private int _gunADV = 0;
+    private int _gunDam = 0;
 
     private void Awake()
     {
-        _knife = GetComponent<Knife>();
-        _playerMove = GetComponent<PlayerMove>();
-        _bow = GetComponent<Bow>();
-        _gun = GetComponent<Gun>();
-        _bullet = GetComponent<Bullet>();
-        _playerHP = GetComponent<PlayerHP>();
+        _knife = GameObject.Find("Knife").GetComponent<Knife>();
+        _playerMove = GameObject.Find("Player").GetComponent<PlayerMove>();
+        _bow = GameObject.Find("Bow").GetComponent<Bow>();
+        _gun = GameObject.Find("Gun").GetComponent<Gun>();
+        _playerHP = GameObject.Find("Player").GetComponent<PlayerHP>();
     }
 
 
     public void KnifeDamageUp()
     {
-        _knife.Damage++;
+        if (_knifeDam <= 7)
+        {
+            _knife.Damage++;
+            _knifeDam++;
+        }
     }
 
     public void KnifeAdvantageUp()
     {
-        _playerMove.Advantage++;
+        if (_knifeADV <= 4)
+        {
+            _playerMove.Advantage++;
+            _knifeADV++;
+        }
     }
 
     public void BowMinPowUp()
     {
-        _bow.MinPow++;
+        if(_bowMin <= 3)
+        {
+            _bow.MinPow++;
+            _bowMin++;
+        }
     }
 
     public void BowMaxPowUp()
     {
-        _bow.MaxPow++;
+        if ( _bowMax <= 3)
+        {
+            _bow.MaxPow++;
+            _bowMax++;
+        }
     }
 
     public void GunAdvantageUp()
     {
-        _gun.Advantage--;
+        if (_gunADV <= 3)
+        {
+            _gun.Advantage--;
+            _gunADV++;
+        }
     }
 
     public void GundamageUp()
     {
-        _bullet.damage++;
+        if (_gunDam <= 2)
+        {
+            _gun.Damage++;
+            _gunDam++;
+        }
     }
 
     public void PlayerHPnowHPUp()
