6ed193061bc672c564c6371b894334fc9bbb7390
hyeyeon059 <kimhyeyeon2006@gmail.com>
2022-07-14 09:40:24 +0900

    플레이어 충돌 감지

--- Assets/01.Scripts/Dev/Hyeyeon/Girin.cs
+++ Assets/01.Scripts/Dev/Hyeyeon/Girin.cs
@@ -4,17 +4,60 @@ using UnityEngine;
 
 public class Girin : MonoBehaviour
 {
-    [SerializeField] private GameObject bulletPrefeb;
-    private Camera camera;
+    [SerializeField] private Transform _player;
+    [SerializeField] private GameObject _bullet;
 
-    void Start()
+    [SerializeField] private float timer;
+    [SerializeField] private float waitingTime;
+
+    private void Start()
     {
-        camera = Camera.main;
-
+        timer = 0.0f;
+        waitingTime = 2f;
     }
 
-    void Update()
+    private void Update()
     {
-        
+        timer += Time.deltaTime;
+
+        if(timer > waitingTime)
+        {
+            timer = 0;
+            shot();
+        }
+    }
+
+    public void shot()
+    {
+        var bl = new List<Transform>();
+
+        for (int i = 0; i < 360; i += 13)
+        {
+            var temp = Instantiate(_bullet);
+
+            Destroy(temp, 5f);
+
+            Vector2 positionsBullet = new Vector2(this.gameObject.transform.position.x, this.gameObject.transform.position.y);
+
+            bl.Add(temp.transform);
+
+            temp.transform.rotation = Quaternion.Euler(0, 0, i);
+        }
+        StartCoroutine(BulletToTarget(bl));
+    }
+
+    IEnumerator BulletToTarget(List<Transform> bl)
+    {
+        yield return new WaitForSeconds(0.5f);
+
+        for (int i = 0; i < bl.Count; i++)
+        {
+            var target_dir = _player.transform.position - bl[i].position;
+
+            var angle = Mathf.Atan2(target_dir.y, target_dir.x) * Mathf.Rad2Deg;
+
+            bl[i].rotation = Quaternion.Euler(0, 0, angle);
+        }
+        bl.Clear();
     }
 }
